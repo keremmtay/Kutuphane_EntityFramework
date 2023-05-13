c8f3efc23761b517918f5963e6fdd318c582e5a5
Kerem <Kerem@ASUS>
2023-05-13 12:29:22 +0300

    Add project files.

---
+++ Odev1_KutuphaneEf/Migrations/20230325214917_mig1.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Odev1_KutuphaneEf.Migrations
+{
+    public partial class mig1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Islemler",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OgrenciId = table.Column<int>(type: "int", nullable: false),
+                    KitapId = table.Column<int>(type: "int", nullable: false),
+                    OduncAldigiTarihi = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IadeEdildiMi = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Islemler", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Kitaplar",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    KitapAdi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SayfaSayisi = table.Column<int>(type: "int", nullable: false),
+                    SilindiMi = table.Column<bool>(type: "bit", nullable: false),
+                    KayitTarihi = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    YazarId = table.Column<int>(type: "int", nullable: false),
+                    BasimTarihi = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    YayınEviId = table.Column<int>(type: "int", nullable: false),
+                    TurId = table.Column<int>(type: "int", nullable: false),
+                    Dil = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    KitapSayisi = table.Column<int>(type: "int", nullable: false),
+                    Cevirmen = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kitaplar", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ogrenciler",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Adi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Soyadi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cinsiyet = table.Column<string>(type: "nvarchar(1)", nullable: false),
+                    Sinif = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DogumTarihi = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AktifMi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    KayitTarihi = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ogrenciler", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Turler",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TurAdi = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Turler", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "YayinEvleri",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    YayinEviAdi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefon = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_YayinEvleri", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Yazarlar",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    YazarAdi = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Yazarlar", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IslemKitap",
+                columns: table => new
+                {
+                    IslemlerId = table.Column<int>(type: "int", nullable: false),
+                    KitaplarId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IslemKitap", x => new { x.IslemlerId, x.KitaplarId });
+                    table.ForeignKey(
+                        name: "FK_IslemKitap_Islemler_IslemlerId",
+                        column: x => x.IslemlerId,
+                        principalTable: "Islemler",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_IslemKitap_Kitaplar_KitaplarId",
+                        column: x => x.KitaplarId,
+                        principalTable: "Kitaplar",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IslemOgrenci",
+                columns: table => new
+                {
+                    IslemlerId = table.Column<int>(type: "int", nullable: false),
+                    OgrencilerId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IslemOgrenci", x => new { x.IslemlerId, x.OgrencilerId });
+                    table.ForeignKey(
+                        name: "FK_IslemOgrenci_Islemler_IslemlerId",
+                        column: x => x.IslemlerId,
+                        principalTable: "Islemler",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_IslemOgrenci_Ogrenciler_OgrencilerId",
+                        column: x => x.OgrencilerId,
+                        principalTable: "Ogrenciler",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "KitapTur",
+                columns: table => new
+                {
+                    KitaplarId = table.Column<int>(type: "int", nullable: false),
+                    TurlerId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KitapTur", x => new { x.KitaplarId, x.TurlerId });
+                    table.ForeignKey(
+                        name: "FK_KitapTur_Kitaplar_KitaplarId",
+                        column: x => x.KitaplarId,
+                        principalTable: "Kitaplar",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_KitapTur_Turler_TurlerId",
+                        column: x => x.TurlerId,
+                        principalTable: "Turler",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "KitapYayinEvi",
+                columns: table => new
+                {
+                    KitaplarId = table.Column<int>(type: "int", nullable: false),
+                    YayinEvleriId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KitapYayinEvi", x => new { x.KitaplarId, x.YayinEvleriId });
+                    table.ForeignKey(
+                        name: "FK_KitapYayinEvi_Kitaplar_KitaplarId",
+                        column: x => x.KitaplarId,
+                        principalTable: "Kitaplar",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_KitapYayinEvi_YayinEvleri_YayinEvleriId",
+                        column: x => x.YayinEvleriId,
+                        principalTable: "YayinEvleri",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "KitapYazar",
+                columns: table => new
+                {
+                    KitaplarId = table.Column<int>(type: "int", nullable: false),
+                    YazarlarId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KitapYazar", x => new { x.KitaplarId, x.YazarlarId });
+                    table.ForeignKey(
+                        name: "FK_KitapYazar_Kitaplar_KitaplarId",
+                        column: x => x.KitaplarId,
+                        principalTable: "Kitaplar",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_KitapYazar_Yazarlar_YazarlarId",
+                        column: x => x.YazarlarId,
+                        principalTable: "Yazarlar",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IslemKitap_KitaplarId",
+                table: "IslemKitap",
+                column: "KitaplarId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IslemOgrenci_OgrencilerId",
+                table: "IslemOgrenci",
+                column: "OgrencilerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_KitapTur_TurlerId",
+                table: "KitapTur",
+                column: "TurlerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_KitapYayinEvi_YayinEvleriId",
+                table: "KitapYayinEvi",
+                column: "YayinEvleriId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_KitapYazar_YazarlarId",
+                table: "KitapYazar",
+                column: "YazarlarId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IslemKitap");
+
+            migrationBuilder.DropTable(
+                name: "IslemOgrenci");
+
+            migrationBuilder.DropTable(
+                name: "KitapTur");
+
+            migrationBuilder.DropTable(
+                name: "KitapYayinEvi");
+
+            migrationBuilder.DropTable(
+                name: "KitapYazar");
+
+            migrationBuilder.DropTable(
+                name: "Islemler");
+
+            migrationBuilder.DropTable(
+                name: "Ogrenciler");
+
+            migrationBuilder.DropTable(
+                name: "Turler");
+
+            migrationBuilder.DropTable(
+                name: "YayinEvleri");
+
+            migrationBuilder.DropTable(
+                name: "Kitaplar");
+
+            migrationBuilder.DropTable(
+                name: "Yazarlar");
+        }
+    }
+}
---
+++ Odev1_KutuphaneEf/Program.cs
@@ -0,0 +1,86 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("Hello, World!");
+
+public class Yazar
+{
+    public int Id { get; set; }
+    public string YazarAdi { get; set; }
+    public ICollection<Kitap> Kitaplar { get; set; }
+}
+
+public class YayinEvi
+{ 
+    public int Id { get; set; }
+    public string YayinEviAdi { get; set; }
+    public string Telefon { get; set; }
+    public ICollection<Kitap> Kitaplar { get; set; }
+}
+
+public class Tur
+{
+    public int Id { get; set; }
+    public string TurAdi { get; set; }
+    public ICollection<Kitap> Kitaplar { get; set; }
+}
+
+public class Ogrenci
+{
+    public int Id { get; set; }
+    public string Adi { get; set; }
+    public string Soyadi { get; set; }
+    public char Cinsiyet { get; set; }
+    public string Sinif { get; set; }
+    public DateTime DogumTarihi { get; set; }
+    public string AktifMi { get; set; }
+    public DateTime KayitTarihi { get; set; }
+    public ICollection<Islem> Islemler { get; set; }
+}
+
+public class Kitap
+{
+    public int Id { get; set; }
+    public string KitapAdi { get; set; }
+    public int SayfaSayisi { get; set; }
+    public bool SilindiMi { get; set; }
+    public DateTime KayitTarihi { get; set; }
+    public int YazarId { get; set; }
+    public DateTime BasimTarihi { get; set; }
+    public int YayınEviId { get; set; }
+    public int TurId { get; set; }
+    public string Dil { get; set; }
+    public int KitapSayisi { get; set; }
+    public string Cevirmen { get; set; }
+    public ICollection<Yazar> Yazarlar { get; set; }
+    public ICollection<YayinEvi> YayinEvleri { get; set; }
+    public ICollection<Tur> Turler { get; set; }
+    public ICollection<Islem> Islemler { get; set; }
+}
+
+public class Islem
+{
+    public int Id { get; set; }
+    public int OgrenciId { get; set; }
+    public int KitapId { get; set; }
+    public DateTime OduncAldigiTarihi { get; set; }
+    public bool IadeEdildiMi { get; set; }
+    public ICollection<Ogrenci> Ogrenciler { get; set; }
+    public ICollection<Kitap> Kitaplar { get; set; }
+}
+
+
+public class Context : DbContext
+{
+    public DbSet<Ogrenci> Ogrenciler { get; set; }
+    public DbSet<Islem> Islemler { get; set; }
+    public DbSet<Kitap> Kitaplar { get; set; }
+    public DbSet<Tur> Turler { get; set; }
+    public DbSet<Yazar> Yazarlar { get; set; }
+    public DbSet<YayinEvi> YayinEvleri { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=ASUS;Database=EfOdevKutuphane;Integrated Security=true");
+    }
+}
